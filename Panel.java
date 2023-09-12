a6af24c490d1c0748ea3b98e9e65ee14d86f27dc
MasonWendel <masonwendel@outlook.com>
2023-09-12 14:32:54 -0500

    Updating panel

--- Panel.java
+++ Panel.java
@@ -2,16 +2,21 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+
 import java.awt.Dimension;
 
 public class Panel extends JPanel {
 	private static final Dimension PANEL_SIZE = new Dimension(200, 100);
 	
 	public Panel(){
-		this.setSize(200,200);
+		this.setBounds(200, 200, 200, 200);
 		this.setBackground(Color.GREEN);
+		this.setBorder(BorderFactory.createRaisedBevelBorder());
 	}
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -19,9 +24,6 @@ public class Panel extends JPanel {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 			RenderingHints.VALUE_ANTIALIAS_ON);
-		for(int i = 0; i<200 ; i++){
-			g.fillOval(i, i, 30, 30);
-		}
 		
 	}
 
